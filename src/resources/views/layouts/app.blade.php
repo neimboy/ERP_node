b1025cd040ad33e6bd76bf86ab16a8921e97c34c
Jeanpool <magalycastro040319@gmail.com>
2026-05-05 01:22:48 -0600

    correccion

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -11,12 +11,8 @@
 </head>
 
 <body class="bg-gray-100">
+    <nav class="bg-white shadow-lg" x-data="{ openContabilidad: false, openAdmin: false, openInventario: false, openVentas: false }">
 
-<<<<<<< HEAD
-    <nav class="bg-white shadow-lg" x-data="{ openContabilidad: false, openAdmin: false, openInventario: false }">
-=======
-    <nav class="bg-white shadow-lg" x-data="{ openContabilidad: false, openAdmin: false, openVentas: false }">
->>>>>>> origin/main
         <div class="max-w-7xl mx-auto px-4">
             <div class="flex justify-between h-16">
 
